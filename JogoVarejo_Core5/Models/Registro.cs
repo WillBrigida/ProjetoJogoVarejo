0132d12d9e76f6e69c95b6c88773882db3cbd448
will <will.brigida@winsizing.com.br>
2020-08-28 07:26:40 -0300

    Testando layouts

---
+++ JogoVarejo_Core5/Models/Registro.cs
@@ -0,0 +1,9 @@
+namespace JogoVarejo_Core5
+{
+    public class Registro
+    {
+        public string Grupo { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+    }
+}
