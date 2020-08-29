eb183014ea7ce0fe00f7f012955bc28abf7e7ea3
will <will.brigida@winsizing.com.br>
2020-08-29 12:58:06 -0300

    Ajustes e melhorias no layout

---
+++ JogoVarejo_Server/Shared/Models/Grupo.cs
@@ -0,0 +1,12 @@
+namespace JogoVarejo_Server.Shared.Models
+{
+    public class Grupo
+    {
+        public int GrupoId { get; set; }
+        public int GrupoOperadorId { get; set; }
+        public string Como { get; set; }
+        public string Quando { get; set; }
+    }
+
+   
+}
