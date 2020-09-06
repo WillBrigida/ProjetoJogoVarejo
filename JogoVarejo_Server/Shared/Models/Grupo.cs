f59d50f7154dd65bf76a2d24fc0b0176285c2efc
Will Brigida <will.brigida@winsizing.com.br>
2020-09-06 03:44:15 -0300

    Iniciando criacao grupo

--- JogoVarejo_Server/Shared/Models/Grupo.cs
+++ JogoVarejo_Server/Shared/Models/Grupo.cs
@@ -6,5 +6,6 @@ namespace JogoVarejo_Server.Shared.Models
         public int GrupoOperadorId { get; set; }
         public string Quando { get; set; }
         public string Quanto { get; set; }
+        ApplicationUser ApplicationUser { get; set; }
     }
 }
